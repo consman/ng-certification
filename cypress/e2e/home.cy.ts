2750df97793889117daed010e6e4d646afdc164b
bob <bob.dev.oak@gmail.com>
2023-02-01 20:57:56 -0800

    intergation w cyress

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,50 @@
+describe("Test GDG Raffle App", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4200");
+  });
+
+  it("Shows the title on the home page.",()=>{
+    cy.title().should("eq","ng-certification app is running!");
+  });
+
+  it("Have title in the first box of Add a location",()=>{
+    cy.get("[data-test='addLocationBoxTitle']").should("include.text","Add a location");
+  });
+
+  it("searches for a location",()=>{
+    cy.get("[data-test='locationField']").type("55121");
+    cy.get("[data-test='searchButton']").click();
+    cy.get("[data-test='searchResultTitle']").should("include.text","Saint Paul");
+  });
+
+  it("should close the result",()=>{
+    cy.get("[data-test='locationField']").type("55121");
+    cy.get("[data-test='searchButton']").click();
+    cy.get("[data-test='searchResultTitle']").should("include.text","Saint Paul");
+    cy.get("[data-test='closeSearchResult']").click();
+    cy.get("[data-test='searchResultTitle']").should("not.exist");
+  });
+
+  it("should provide the 5-day forecast ",()=>{
+    cy.get("[data-test='locationField']").type("55121");
+    cy.get("[data-test='searchButton']").click();
+    cy.get("[data-test='searchResultTitle']").should("include.text","Saint Paul");
+    cy.get("[data-test='fiveDayForecastLink']").click();
+    cy.get("[data-test='tdForecastItem']").eq(4).should("include.text","day");
+  });
+
+  it("should take you back to the main page from the 5-day forecast ",()=>{
+    cy.get("[data-test='locationField']").type("55121");
+    cy.get("[data-test='searchButton']").click();
+    cy.get("[data-test='searchResultTitle']").should("include.text","Saint Paul");
+    cy.get("[data-test='fiveDayForecastLink']").click();
+
+    cy.get("[data-test='backToMainPageButton']").click();
+    cy.get("[data-test='addLocationBoxTitle']").should("include.text","Add a location");
+    cy.get("[data-test='searchResultTitle']").eq(0).should("include.text","Saint Paul");
+
+  });
+
+
+
+})
