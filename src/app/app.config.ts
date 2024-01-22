ba5e93e408b89a56d09a5922826a84625ef14e3c
bob <bob.dev.oak@gmail.com>
2024-01-22 11:52:06 -0800

    Got Junits and Cypress to work with NON-Prod but still struggling with making PROD work. See comments in provders in array in  src/app/app.config.ts. The first one, for weatherServiceFactory, worked in app.module.ts in V16 but not in Version 17 in app.config.ts .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,9 +10,9 @@ import { environment } from '../environments/environment';
 export const appConfig: ApplicationConfig = {
   
   providers: [provideRouter(routes),
-    {provide: WeatherService, useFactory: (isProd: boolean, http: HttpClient)=> weatherServiceFactory(isProd, http), deps: ['IS_PROD_ENVIRONMENT',HttpClient]}, // works with non-prod, but not PROD
-    //{provide: WeatherService, useFactory: weatherServiceFactory, deps: ['IS_PROD_ENVIRONMENT','http']},
-    //{provide: WeatherService, useFactory: (isProd: boolean, http: HttpClient)=> weatherServiceFactory(isProd, http), deps: ['IS_PROD_ENVIRONMENT','http']},
+    //{provide: WeatherService, useFactory: (isProd: boolean, http: HttpClient)=> weatherServiceFactory(isProd, http), deps: ['IS_PROD_ENVIRONMENT',HttpClient]}, // works well for everything in V16 - But in 17 unit tests work with non-prod, but not PROD or cypress which crash with ERROR NullInjectorError: R3InjectorError(Standalone[n])[n -> n -> n -> _HttpClient -> _HttpClient]: NullInjectorError: No provider for _HttpClient!
+    {provide: WeatherService, useFactory: weatherServiceFactory, deps: ['IS_PROD_ENVIRONMENT','http']}, // V17: units tests and cypress both work, but not PROD which crashes with ERROR TypeError: this.http.get is not a function
+    //{provide: WeatherService, useFactory: (isProd: boolean, http: HttpClient)=> weatherServiceFactory(isProd, http), deps: ['IS_PROD_ENVIRONMENT','http']}, // v!& works with non-prod, unit tests and cypress work ( but switching to PROD causes FivedayforecastComponent junits to fail and cypress to fail with timeouts and manual testing gives ERROR TypeError: this.http.get is not a function ) 
     {provide: 'IS_PROD_ENVIRONMENT', useValue: environment.production},
   {provide: 'http', useValue: HttpClient}
   ]
