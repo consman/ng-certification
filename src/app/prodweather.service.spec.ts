1d453bbba33355a8b13a764959b3c0fe6fcf6d6b
bob <bob.dev.oak@gmail.com>
2024-12-30 22:13:17 -0800

    fix name of src/app/Prodweather.service.ts

--- src/app/prodweather.service.spec.ts
+++ src/app/prodweather.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProdweatherService } from './prodweather.service';
+import { ProdweatherService } from './Prodweather.service';
 
 import {HttpClient} from '@angular/common/http';
 import {RAWFORECASTS, RAWLOCATIONS} from './mock-data'; 
