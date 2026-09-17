30de0094ce34473dae9ec1fc672d7200761ed041
bob <bob.dev.oak@gmail.com>
2026-09-17 15:19:49 -0700

    Update to Angular Version 22 after fixing bug 22.001.

--- src/app/prodweather.ts
+++ src/app/prodweather.ts
@@ -20,12 +20,12 @@ export class Prodweather extends Weather {
     // TODO update to use the GeoCodeApi to get the lonitude/lat from the zip code, the the onecall should have everything.
     //  https://api.openweathermap.org/geo/1.0/zip?zip=95630,US&zappid=
     // {"zip":"95630","name":"Folsom","lat":38.6709,"lon":-121.1529,"country":"US"}
-    return this.http.get<LocationT>('https://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&appid=7bba6087ba8a1265ba39fb831ec12cde');
+    return this.http.get<LocationT>('https://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&appid=');
   }
   
   //New OneCall Service:
   override getFiveDayForecastFromService(lat: number, lon: number): Observable<Forecast> {
-    return this.http.get<Forecast>('https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=7bba6087ba8a1265ba39fb831ec12cde');
+    return this.http.get<Forecast>('https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=');
   }
 
 }
