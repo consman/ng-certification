e78ad2218c599ad07df62a4ba7f77bba79044d05
bob <bob.dev.oak@gmail.com>
2026-09-17 20:41:01 -0700

    Update to Angular Version 22 after fixing bug 22.002.

--- src/app/prodweather.ts
+++ src/app/prodweather.ts
@@ -6,6 +6,8 @@ import {Observable} from 'rxjs';
 import { Forecast } from './forecast';
 import { Weather } from './weather';
 
+declare const W_APP_ID: string;
+
 @Service()
 export class Prodweather extends Weather {
 /*
@@ -13,19 +15,21 @@ export class Prodweather extends Weather {
     super();
   }
 */
-    http = inject (HttpClient);
+  http = inject (HttpClient);
+
+  wAppId = W_APP_ID;
     
-    override getLocationFromService(zipcode: string): Observable<LocationT> {
+  override getLocationFromService(zipcode: string): Observable<LocationT> {
     console.log('Going for PROD weather service getLocationFromService and the zipcode is: ' + zipcode);   
     // TODO update to use the GeoCodeApi to get the lonitude/lat from the zip code, the the onecall should have everything.
     //  https://api.openweathermap.org/geo/1.0/zip?zip=95630,US&zappid=
     // {"zip":"95630","name":"Folsom","lat":38.6709,"lon":-121.1529,"country":"US"}
-    return this.http.get<LocationT>('https://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&appid=');
+    return this.http.get<LocationT>('https://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&appid='+this.wAppId);
   }
   
   //New OneCall Service:
   override getFiveDayForecastFromService(lat: number, lon: number): Observable<Forecast> {
-    return this.http.get<Forecast>('https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=');
+    return this.http.get<Forecast>('https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid='+this.wAppId);
   }
 
 }
